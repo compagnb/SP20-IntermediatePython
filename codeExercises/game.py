ae7c9533b8cb35613935a65809eee36f3804a2df
Barbara Compagnoni <compagnb@newschool.edu>
2020-05-02 11:07:58 -0400

    Updated with todays code!

--- codeExercises/game.py
+++ codeExercises/game.py
@@ -3,12 +3,30 @@ import random
 import wk3_gameClass
 ## import gameClass
 
+# variables that will make our lives easier
+bLength = 600
+bHeight = 600
+maxInvader = 10
+maxVelocity = 10
+invaders = [ ]
+
+# the instances we are creating! 
 wn =  turtle.Screen()
+logan = wk3_gameClass.Player("blue","square", bLength, bHeight)
+border = wk3_gameClass.Border("blue", bLength, bHeight, 3)
+
+# create instances in an array
+#for i in range(maxInvader):
+ #   invaders.append(wk3_gameClass.Invader("red", "triangle", random.randint(-290, 290), random.randint(-290, 290), bLength, bHeight, 3))
+
+# all our window stuff
 wn.title("Space Invaders!!!")
 ## Background Images!!!!
 
-logan = wk3_gameClass.Player("blue","square")
+# all our border stuff
+border.drawBorder()
 
+# all our player control stuff
 turtle.listen()
 turtle.onkey(logan.turnLeft,"Left")
 turtle.onkey(logan.turnRight,"Right")
@@ -19,5 +37,6 @@ turtle.onkey(logan.slowDown,"Down")
 while True:
     wn.update()
     logan.move()
+
     
 ##compagnb@gmail.com
--- codeExercises/wk3_gameClass.py
+++ codeExercises/wk3_gameClass.py
@@ -3,20 +3,46 @@ import random
 maxVelocity = 10
 
 ## Border
-
+class Border(turtle.Turtle):
+    def __init__(self, bColor, bLength, bHeight, bThickness):
+        turtle.Turtle.__init__(self)
+        self.color(bColor)
+        self.pensize(bThickness)
+        self.speed(0)
+        self.bLength = bLength
+        self.bHeight = bHeight
+        self.penup()
+        self.hideturtle()
+    def drawBorder(self):
+        self.setposition(-1*self.bLength/2, self.bHeight/2)
+        self.pendown()
+        for s in range(4):
+            if s % 2 == 0:
+                self.forward(self.bHeight)  
+            else:
+                self.forward(self.bLength)
+            self.right(90)
+            
 ## Player
 class Player(turtle.Turtle):
     ## Player Image!!!
-    def __init__(self, plColor, plShape):
+    def __init__(self, plColor, plShape, bLength, bHeight):
         turtle.Turtle.__init__(self)
         self.up()
         self.speed(0)
         self.color(plColor)
         self.shape(plShape)
+        self.bLength = bLength
+        self.bHeight = bHeight
         self.velocity = 1
     def move(self):
-        # Boundary
         self.forward(self.velocity)
+        #Boundary Check on the X axis
+        if self.xcor() > self.bLength/2-10 or self.xcor() < (self.bLength/2-10)*-1:
+            self.right(180)
+        #Boundary Check on the Y axis
+        if self.ycor() > self.bHeight/2-10 or self.ycor() < (self.bHeight/2 -10)*-1:
+           self.right(180)                  
     def turnLeft(self):
         self.left(30)
     def turnRight(self):
@@ -34,26 +60,47 @@ class Player(turtle.Turtle):
 
 ## Space Invader
 class Invader(turtle.Turtle):
-    def __init__(self, color, xPos, yPos):
+    def __init__(self, inColor, inShape, xPos, yPos, bLength, bHeight, inVel):
         turtle.Turtle.__init__(self)
-        self.color(str(color))
+        self.color(inColor)
+        self.shape(inShape)
+        self.bLength = bLength
+        self.bHeight = bHeight
         self.up()
-        self.goto(xPos, yPos);
+        self.goto(xPos, yPos)
         self.speed(0)
+        self.velocity = inVel
     def move(self):
         self.forward(1)
+        #Boundary Check on the X axis
+        if self.xcor() > self.bLength/2-10 or self.xcor() < (self.bLength/2-10)*-1:
+            self.right(180)
+        #Boundary Check on the Y axis
+        if self.ycor() > self.bHeight/2-10 or self.ycor() < (self.bHeight/2 -10)*-1:
+           self.right(180) 
     def shoot(self):
         pass
     
 
 ## Quidditch
 class teamPlayer(turtle.Turtle):
-    def __init__(self, color, xPos, yPos):
+    def __init__(self, inColor, inShape, xPos, yPos, bLength, bHeight, inVel):
         turtle.Turtle.__init__(self)
-        self.color(str(color))
+        self.color(inColor)
+        self.shape(inShape)
+        self.bLength = bLength
+        self.bHeight = bHeight
         self.up()
-        self.goto(xPos, yPos);
+        self.goto(xPos, yPos)
+        self.speed(0)
+        self.velocity = inVel
     def move(self):
-        pass
+        self.forward(1)
+        #Boundary Check on the X axis
+        if self.xcor() > self.bLength/2-10 or self.xcor() < (self.bLength/2-10)*-1:
+            self.right(180)
+        #Boundary Check on the Y axis
+        if self.ycor() > self.bHeight/2-10 or self.ycor() < (self.bHeight/2 -10)*-1:
+           self.right(180) 
     def shoot(self):
         pass

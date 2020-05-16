155aef260e5f40b870629d9e6e97cba2028e02fc
Barbara Compagnoni <compagnb@newschool.edu>
2020-05-16 09:08:53 -0400

    update

--- codeExercises/game.py
+++ codeExercises/game.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+import math
 import wk3_gameClass
 ## import gameClass
 
@@ -9,16 +10,26 @@ bHeight = 600
 maxInvader = 10
 maxVelocity = 10
 invaders = [ ]
+count = 0
 
 # the instances we are creating! 
 wn =  turtle.Screen()
+wn.bgcolor("grey")
+# Background pictures should be gifs & pngs
+# wn.bgpic("")
+wn.tracer(0)
 logan = wk3_gameClass.Player("blue","square", bLength, bHeight)
 border = wk3_gameClass.Border("blue", bLength, bHeight, 3)
+scoreObj = wk3_gameClass.Score("blue", bLength, bHeight)
 
 # create instances in an array
-#for i in range(maxInvader):
- #   invaders.append(wk3_gameClass.Invader("red", "triangle", random.randint(-290, 290), random.randint(-290, 290), bLength, bHeight, 3))
-
+for i in range(maxInvader):
+    if count %2 == 0:
+        invaders.append(wk3_gameClass.Invader("red", "triangle", random.randint(-290, 290), random.randint(-290, 290), bLength, bHeight, 3))
+    else:
+        invaders.append(wk3_gameClass.Invader("purple", "triangle", random.randint(-290, 290), random.randint(-290, 290), bLength, bHeight, 3))
+    count= count +1
+    
 # all our window stuff
 wn.title("Space Invaders!!!")
 ## Background Images!!!!
@@ -33,10 +44,53 @@ turtle.onkey(logan.turnRight,"Right")
 turtle.onkey(logan.speedUp,"Up")
 turtle.onkey(logan.slowDown,"Down")
 
+# all our collision detection stuff
+# a2 +b2 = c2
+def isCollision(t1, t2):
+    a = t1.ycor() - t2.ycor()
+    b = t1.xcor() - t2.xcor()
+    aSquared = math.pow(a, 2)
+    bSquared = math.pow(b, 2)
+    cSquared = aSquared + bSquared
+    c= math.sqrt(cSquared)
+    if  c < 20:
+        # there is a collision!!!
+        return True
+    else:
+        # there is no collision!!!!!
+        return False
+
+
 ## this is going to be our game loop.... its never gonna end until we end it!
 while True:
     wn.update()
+
     logan.move()
+
+    for invader in invaders:
+        invader.move()
+        
+        if isCollision(logan, invader):
+            # if isCollision is true do this!
+            # logan.setposition(0,0)
+            invader.setposition(random.randint(-290, 290),random.randint(-290, 290))
+            if invader.inColor == "purple":
+                scoreObj.changeScore(100)
+            else:
+               scoreObj.changeScore(10)               
+
+
+
+
+
+
+
+
+
+    
+
+
+
 
     
 ##compagnb@gmail.com
--- codeExercises/wk3_gameClass.py
+++ codeExercises/wk3_gameClass.py
@@ -2,6 +2,25 @@ import turtle
 import random
 maxVelocity = 10
 
+## Score
+class Score(turtle.Turtle):
+    def __init__(self, sColor, bLength, bHeight):
+        turtle.Turtle.__init__(self)
+        self.color(sColor)
+        self.speed(0)
+        self.hideturtle()
+        self.penup()
+        self.goto(bLength/2 -80, bHeight/2 +10)
+        self.score =0
+    def updateScore(self):
+        self.clear()
+        self.write("Score: {}".format(self.score), False, align="left", font=("Neue Display", 14, "normal"))
+    def changeScore(self, points):
+        self.score = self.score + points
+        self.updateScore()
+    def playSound(self):
+        pass       
+
 ## Border
 class Border(turtle.Turtle):
     def __init__(self, bColor, bLength, bHeight, bThickness):
@@ -63,12 +82,14 @@ class Invader(turtle.Turtle):
     def __init__(self, inColor, inShape, xPos, yPos, bLength, bHeight, inVel):
         turtle.Turtle.__init__(self)
         self.color(inColor)
+        self.inColor = inColor
         self.shape(inShape)
         self.bLength = bLength
         self.bHeight = bHeight
         self.up()
         self.goto(xPos, yPos)
         self.speed(0)
+        self.setheading(random.randint(0, 360))
         self.velocity = inVel
     def move(self):
         self.forward(1)
